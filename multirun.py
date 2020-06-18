9d6374aefeb7c7bed42ad2b3b1afc3cdabb9946f
nikpick <gly.nkt24@gmail.com>
2020-06-18 02:38:02 +0200

    file per runnare quello di silverio

--- multirun.py
+++ multirun.py
@@ -3,4 +3,4 @@ import os
 for file in os.listdir('.'): #elenco elementi in directory
     if os.path.isfile(file): #controllo se elemento e' file
         if file.endswith('.mo'): #prendo solo i .mo
-            os.system("singlerun.py" + " " + file) #run singlerun con argomento l'elemento del for
+            os.system(os.getcwd() + "/" + "singlerun.py" + " ./" + file) #run singlerun con argomento l'elemento del for
