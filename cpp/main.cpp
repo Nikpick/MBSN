512688778955467d7c2bba7029698fc245d1a04f
axsnxs <smanganar@gmail.com>
2020-10-09 17:20:41 +0200

    altra versione con libreria jsoncpp built-in

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,13 +1,22 @@
+/*
+File creato da MDG, SM, BD e NG
 
-//#include <libraries/jsoncpp/include/json/reader.h>
-//include <libraries/jsoncpp>
-//\libraries\jsoncpp\src\lib_json\json_reader.cpp
+NECESSARIO:
+sudo apt-get install libjsoncpp-dev
+*/
 
-#include "./libraries/jsoncpp/src/lib_json/json_tool.h"
-#include "./../../include/json/config.h"
+#include <jsoncpp/json/json.h>
+#include <jsoncpp/json/value.h>
+#include <fstream>
+#include <iostream>
 
 int main(int argc, char const *argv[])
 {
-    
+    std::ifstream file_input("out.json");
+    Json::Reader reader;
+    Json::Value root;
+    reader.parse(file_input, root);
+    std::cout << root;
+
     return 0;
 }
