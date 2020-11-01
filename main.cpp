83732ce05c13053985a3cb6d7a9d9f21b961b010
Nico Frusciante <axisray@live.it>
2020-11-01 20:23:40 +0100

    ulteriori test sui valori del Json::Value e sui Membri

--- main.cpp
+++ main.cpp
@@ -12,8 +12,15 @@ int main(int argc, char const *argv[])
     
     for (auto &member : vec) {
         auto values = jvalue[member.c_str()].toStyledString();
-        auto v = values.c_str();
-        printf("Valori per il membro --%s--: %s\n", member.c_str(), v);
+        auto v1 = values.c_str();
+        //printf("Valori per il membro --%s--: %s\n", member.c_str(), v1);
+
+        for (auto & val : jvalue[member.c_str()]) {
+            auto v2 = val.toStyledString();
+            printf("Singolo valore per --%s--:\n", member.c_str());            
+            printf("%s\n\n", v2.c_str());
+            
+        }
     }
 
 
