4c8589eca411ae76797e360ebe0cc722ca2f1c55
Nico Frusciante <axisray@live.it>
2020-11-01 20:05:05 +0100

    preso in input il json, salvato nella variabile Value ed effettuati i primi test per il parsing

--- cpp/json_reader.cpp
+++ cpp/json_reader.cpp
@@ -1,5 +1,6 @@
 
 #include "../libraries/jsoncpp/json/json.h"
+#include "../libraries/jsoncpp/json/json-forwards.h"
 #include "../libraries/jsoncpp/jsoncpp.cpp"
 #include <fstream>
 #include <iostream>
@@ -7,32 +8,34 @@
 
 using namespace std;
 
-Json::Value read_and_parse(string path) {
-    Json::Value modello;                                            // DICHIARAZIONE VARIABILI
+// Json::Value json_reader(string path) {
+Json::Value json_reader(string path) {
+
+    Json::Value modello;                                                // DICHIARAZIONE VARIABILI
     Json::CharReaderBuilder builder;
-    Json::CharReader* reader = builder.newCharReader();            // ALLOCAZIONE
+    Json::CharReader* reader = builder.newCharReader();                 // ALLOCAZIONE
     std::string line, text, errors;                             
 
-    std::ifstream file(path, std::ifstream::binary);                // LETTURA DEL JSON IN INPUT
-    while (std::getline(file, line)) {                              // CONVERSIONE DEL JSON IN STRINGA
+    std::ifstream file(path, std::ifstream::binary);                    // LETTURA DEL JSON IN INPUT
+    while (std::getline(file, line)) {                                  // CONVERSIONE DEL JSON IN STRINGA
         text.append(line);
-        //printf("%s", line.c_str());                               // TEST DI LETTURA SU TERMINALE
+        //printf("%s", line.c_str());                                   // TEST DI LETTURA SU TERMINALE
     }
 
-    bool parsingSuccessful = reader->parse(                         // PARSING DEL JSON IN VALUE &
-        text.c_str(),                                               // ASSEGNAZIONE A UN BOOLEANO
+    bool parsingSuccessful = reader->parse(                             // PARSING DEL JSON IN VALUE &
+        text.c_str(),                                                   // ASSEGNAZIONE A UN BOOLEANO
         text.c_str() + text.size(),
         &modello,
         &errors
     );
+
+    delete reader;
  
-    if ( !parsingSuccessful ) {                                     // CHECK SE ESITO NEGATIVO PARSING
-        std::cout << "Failed to parse the JSON, errors:" 
-                  << std::endl;
+    if ( !parsingSuccessful ) {                                         // CHECK SE ESITO NEGATIVO PARSING
+        std::cout << "Failed to parse the JSON, errors:" << std::endl;
         std::cout << errors << std::endl;
-        return 0;
     } else {
-        printf("%s\n", path.c_str());
+        printf("\nsuccess\n");
     }
 
     return modello;
--- main.cpp
+++ main.cpp
@@ -2,10 +2,29 @@
 #include <iostream>
 #include <Model/Module.h>
 #include <json/json.h>
+#include <json/json-forwards.h>
 #include <json_reader.cpp>
 
 int main(int argc, char const *argv[])
 {
+    auto jvalue = json_reader("../out.json");
+    auto vec = jvalue.getMemberNames();
+    
+    for (auto &member : vec) {
+        auto values = jvalue[member.c_str()].toStyledString();
+        auto v = values.c_str();
+        printf("Valori per il membro --%s--: %s\n", member.c_str(), v);
+    }
+
+
+
+
+    /*
+    // FOR TESTING PURPOSES
+    auto strJvalue = jvalue.toStyledString();
+    auto str = strJvalue.c_str();
+    printf("%s", str);
+    */
 
     return 0;
 }
