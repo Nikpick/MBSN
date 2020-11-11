0baa76a70efd6428db869b513683c3fe14b030cf
Silverio-Man <smanganar@gmail.com>
2020-11-11 20:48:13 +0100

    Proseguimento del parsing

--- cpp/parser.cpp
+++ cpp/parser.cpp
@@ -1,6 +1,7 @@
 
-
+//#include "../headers/parser.h"
 #include <rapidxml.hpp>
+// #include "../libraries/rapidxml-1.13/rapidxml.hpp"
 #include <string.h>
 #include <iostream>
 #include <fstream>
@@ -9,26 +10,35 @@ using namespace Modeling;
 using namespace rapidxml;
 using namespace std;
 
+//SET METHODS
+void setVariables(Module* module, xml_document<>* doc, xml_node<>* node) {
+    cout << node->name() << endl;
+    node = node->next_sibling();
+    if(node) setVariables(module, doc, node);
+}
 
+//GET METHODS
+void getVariables(Module* module, xml_document<>* doc, xml_node<>* node) {
+    cout << node->name() << endl;
+}
 
 void parser(Module* module, xml_document<>* doc, xml_node<>* root) {
+    xml_node<>* node = root->first_node();
+    while(node) {
+        auto nodeName = node->name();
+        if(strcmp(nodeName, "variables") == 0) {
+            //cout << "#" << nodeName << "#" << endl;
+            cout << "ciao " << endl;
+            setVariables(module, doc, node);
     
-    shared_ptr<Variable> variable;
-    string test = "test";
+        } 
+    //     else if(nodeName == "equations") {
 
-    //setters
-    // module.setName("ciao");
-    // module.addExpression(test);
-    // module->addModExpr(variable);
-
-    // //getters
-    // module.getName();
-    // module.getOperations();
-    // module.getVars();
-    // module.getModExpr();
-    // module.getOperations();
-    // module.getExpression();
-
-    return;
+    //     }
+    //     else if(nodeName == "functions") {
+            
+    //     }
+        node = node->next_sibling();
+    }
 }
 
