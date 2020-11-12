3d62c3738d93fa3d766034e51bd2b4efae7fc040
Silverio-Man <smanganar@gmail.com>
2020-11-12 21:52:25 +0100

    Avanzamento nel parsing delle variabili

--- cpp/parser.cpp
+++ cpp/parser.cpp
@@ -10,34 +10,82 @@ using namespace Modeling;
 using namespace rapidxml;
 using namespace std;
 
-//SET METHODS
-void setVariables(Module* module, xml_document<>* doc, xml_node<>* node) {
+//GETTERS METHODS
+void getEquations(Module* module, xml_document<>* doc, xml_node<>* node) {
     cout << node->name() << endl;
-    node = node->next_sibling();
-    if(node) setVariables(module, doc, node);
 }
 
-//GET METHODS
+void getFunctionss(Module* module, xml_document<>* doc, xml_node<>* node) {
+    cout << node->name() << endl;
+}
+
 void getVariables(Module* module, xml_document<>* doc, xml_node<>* node) {
     cout << node->name() << endl;
 }
 
-void parser(Module* module, xml_document<>* doc, xml_node<>* root) {
+//SETTERS METHODS
+
+void setEquations(Module* module, xml_node<>* node) {
+    
+}
+
+void setFunctions(Module* module, xml_node<>* node) {
+    
+}
+
+void setAliasVariable(Module* module, xml_node<>* node) {
+    cout << node->first_attribute("name")->value() << endl;
+}
+
+void setOrderedVariable(Module* module, xml_node<>* node) {
+    cout << "Variabile: " << node->first_attribute("name")->value() << endl;
+
+    std::shared_ptr<Variable> variable;
+
+    //name
+    string name = node->first_attribute("name")->value();
+    
+    //tipo (reale, intero ecc)
+    string type = node->first_attribute("type")->value();
+
+    //valore
+}
+
+void setVariables(Module* module, xml_node<>* node) {
+    cout << "# inizio set variable" << endl;
+
+    xml_node<>* variableNode = node->first_node()->first_node();
+    if(strcmp(node->name(), "orderedVariables") == 0) {
+        while(variableNode) { // Per ogni <variable> in <variablesList>
+            setOrderedVariable(module, variableNode);
+
+            variableNode = variableNode->next_sibling();
+        }
+    }
+    else if(strcmp(node->name(), "aliasVariables") == 0) {
+        
+    }
+    
+    cout << "# fine set variable" << endl;
+    // chiamata riorsiva se ci sono nodi fratelli
+    if(node->next_sibling()) setVariables(module, node->next_sibling());
+}
+
+void parser(Module* module, xml_node<>* root) {
     xml_node<>* node = root->first_node();
     while(node) {
         auto nodeName = node->name();
         if(strcmp(nodeName, "variables") == 0) {
-            //cout << "#" << nodeName << "#" << endl;
-            cout << "ciao " << endl;
-            setVariables(module, doc, node);
-    
+            setVariables(module, node->first_node());
         } 
-    //     else if(nodeName == "equations") {
-
-    //     }
-    //     else if(nodeName == "functions") {
-            
-    //     }
+        else if(strcmp(nodeName, "equations") == 0) {
+            setEquations(module, node->first_node());
+        }
+        else if(strcmp(nodeName, "functions") == 0) {
+            setFunctions(module, node->first_node());
+        }
+        else cout << nodeName << " not found." << endl;
+        
         node = node->next_sibling();
     }
 }
--- main.cpp
+++ main.cpp
@@ -10,10 +10,10 @@
 #include <rapidxml.hpp>
 #include "parser.cpp"
 
+
 using namespace std;
 using namespace rapidxml;
 using namespace Modeling;
-
 
 
 int main(int argc, char const *argv[])
@@ -34,13 +34,14 @@ int main(int argc, char const *argv[])
     doc.parse<0>(xmlAsCharArray);
     // Fine del parsing: la variabile doc contiene il dizionario
 
-    //cout << "First node name: " << firstNodeName->name() << endl;
 
     // auto mod = std::make_shared<Modeling::Module>("foo");
-    auto module = Module("ciao");
+    auto module = Module("system");
     auto root = doc.first_node("dae");
 
-    parser(&module, &doc, root);
+    cout << "First node name: " << root->name() << endl;
+    
+    parser(&module, root);
     
     return 0;
 }
