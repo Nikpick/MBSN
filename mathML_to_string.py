f2fed6bedf239663a5d37ea3faabad1e00fa43f1
Nikpick <gly.nkt24@gmail.com>
2021-01-26 00:59:24 +0100

    iniziato matML_to_string

--- mathML_to_string.py
+++ mathML_to_string.py
@@ -1,54 +1,51 @@
-
 import libsbml
-
-expected = "1 + f(x)"
- 
-xml = "<?xml version='1.0' encoding='UTF-8'?>"\
-       "<math xmlns='http://www.w3.org/1998/Math/MathML'>"\
-       "  <apply> <plus/> <cn> 1 </cn>"\
-       "                  <apply> <ci> f </ci> <ci> x </ci> </apply>"\
-       "  </apply>"\
-       "</math>"
+import os
+from xml.dom import minidom
+import xml.etree.ElementTree as ET
 
 
-xml1 = "<?xml version='1.0' encoding='UTF-8'?>"\
-      "<math xmlns=\"http://www.w3.org/1998/Math/MathML\">"\
-      "  <apply>"\
-      "    <equivalent/>"\
-            "    <apply>"\
-            "      <diff/>"\
-            "      <ci> myclass.x "\
-            "      </ci>"\
-            "    </apply>"\
-                                    "    <apply>"\
-                                    "      <times/>"\
-                                    "      <ci> $cse1 "\
-                                    "      </ci>"\
-                                    "      <apply>"\
-                                    "        <times/>"\
-                                    "        <ci> Params.a "\
-                                    "        </ci>"\
-                                    "        <ci> myclass.y "\
-                                    "        </ci>"\
-                                    "      </apply>"\
-                                    "    </apply>"\
-      "  </apply>"\
-      "</math>"
-      
+listXml = [] # lista degli xml presenti nella directory
 
- 
-ast    = libsbml.readMathMLFromString(xml1)
-result = libsbml.formulaToL3String(ast)
+for files in os.listdir(os.getcwd()): # elenco elementi in dir corrente
+      if os.path.isfile(os.getcwd() + "/" + files): # controllo se elemento e' file
+            if files.endswith('.xml'): # prendo solo gli .xml
+                  listXml = listXml + [files] # mi salvo il nome del xml nella lista
 
-print(result)
-# if (result == expected):
-#     print(result)
-# else:
-#     print("Mismatch after readMathMLFromString()")
 
-new_mathml = libsbml.parseL3Formula(result)
-new_string = libsbml.writeMathMLToString(new_mathml)
+for nameXml in listXml:
+      fullPath = os.getcwd() + '/' + nameXml #path del file xml
+      tree = ET.parse(fullPath) #albero con i primi figli del file xml
+      root = tree.getroot() #radice del albero
+      print(root)
+      # find all "item" objects and print their "name" attribute
 
-print(new_string)
-# print("Result of writing AST to string: ")
-# print(new_string)
+
+
+
+
+
+
+#expected = "1 + f(x)"
+# 
+#xml = "<math xmlns='http://www.w3.org/1998/Math/MathML'>"\
+#      "  <apply> <plus/> <cn> 1 </cn>"\
+#      "                  <apply> <ci> f </ci> <ci> x </ci> </apply>"\
+#      "  </apply>"\
+#      "</math>"
+#
+# 
+#ast    = libsbml.readMathMLFromString(xml)
+#result = libsbml.formulaToL3String(ast)
+#
+#print(result)
+## if (result == expected):
+##     print(result)
+## else:
+##     print("Mismatch after readMathMLFromString()")
+#
+#new_mathml = libsbml.parseL3Formula(result)
+#new_string = libsbml.writeMathMLToString(new_mathml)
+#
+#print(new_string)
+## print("Result of writing AST to string: ")
+## print(new_string)
